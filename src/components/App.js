d5b5164480b06b0b7bcb65b2443e6aa9ab8d0eea
xaviguasch <xaviguasch@gmail.com>
2019-12-28 15:22:25 +0100

    toyed with forms and state

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,68 @@
 import React, { useState } from 'react'
 
 function App() {
-    const [headingText, setHeadingText] = useState('Hello')
+    const [name, setName] = useState('')
+    const [headingText, setHeading] = useState('')
 
-    const [isMousedOver, setMouseOver] = useState(false)
+    function handleChange(event) {
+        setName(event.target.value)
+    }
 
     function handleClick() {
-        setHeadingText('Submited!')
-    }
-
-    function handleMouseOver() {
-        setMouseOver(true)
-    }
-
-    function handleMouseOut() {
-        setMouseOver(false)
+        setHeading(name)
     }
 
     return (
         <div className='container'>
-            <h1>{headingText}</h1>
-            <input type='text' placeholder="What's your name?" />
-            <button
-                style={{
-                    backgroundColor: isMousedOver ? 'black' : 'white'
-                }}
-                onClick={handleClick}
-                onMouseOver={handleMouseOver}
-                onMouseOut={handleMouseOut}
-            >
-                Submit
-            </button>
+            <h1>Hello {headingText}</h1>
+            <input
+                onChange={handleChange}
+                type='text'
+                placeholder="What's your name?"
+                value={name}
+            />
+            <button onClick={handleClick}>Submit</button>
         </div>
     )
 }
 
 export default App
+
+// import React, { useState } from 'react'
+
+// function App() {
+//     const [headingText, setHeadingText] = useState('Hello')
+
+//     const [isMousedOver, setMouseOver] = useState(false)
+
+//     function handleClick() {
+//         setHeadingText('Submited!')
+//     }
+
+//     function handleMouseOver() {
+//         setMouseOver(true)
+//     }
+
+//     function handleMouseOut() {
+//       setMouseOver(false)
+//     }
+
+//     return (
+//         <div className='container'>
+//             <h1>{headingText}</h1>
+//             <input type='text' placeholder="What's your name?" />
+//             <button
+//                 style={{
+//                     backgroundColor: isMousedOver ? 'black' : 'white'
+//                 }}
+//                 onClick={handleClick}
+//                 onMouseOver={handleMouseOver}
+//                 onMouseOut={handleMouseOut}
+//             >
+//                 Submit
+//             </button>
+//         </div>
+//     )
+// }
+
+// export default App
