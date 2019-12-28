3bd0625281dc10b663ae75a3879a6ddbdddcd159
xaviguasch <xaviguasch@gmail.com>
2019-12-28 15:40:13 +0100

    refactored previous code using forms

--- src/components/App.js
+++ src/components/App.js
@@ -8,20 +8,24 @@ function App() {
         setName(event.target.value)
     }
 
-    function handleClick() {
+    function handleClick(event) {
         setHeading(name)
+
+        event.preventDefault()
     }
 
     return (
         <div className='container'>
             <h1>Hello {headingText}</h1>
-            <input
-                onChange={handleChange}
-                type='text'
-                placeholder="What's your name?"
-                value={name}
-            />
-            <button onClick={handleClick}>Submit</button>
+            <form onSubmit={handleClick}>
+                <input
+                    onChange={handleChange}
+                    type='text'
+                    placeholder="What's your name?"
+                    value={name}
+                />
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
